b9473ce8a30740e33c778d8532d2045003254bf9
Gloria <gloriajoanlai@gmail.com>
2020-08-10 19:33:14 -0400

    some css done, and some react

--- src/App.js
+++ src/App.js
@@ -7,23 +7,73 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      
+      queues: [],
+      userAsk: ''
     }
-    console.log('constructor', this)
   }
 
   componentDidMount() {
     const dbRef = firebase.database().ref();
 
     dbRef.on('value', (response) => {
-      console.log('response', response)
+      console.log('response', response.val())
+
+      const data = response.val();
+
+      const helpcueUpdated = [];
+
+      for (let propertyName in data) {
+        const waitingForHelp = {
+          id: propertyName,
+          question: data[propertyName]
+        }
+        helpcueUpdated.push(waitingForHelp)
+      }
+      console.log('helpcueUpdated', helpcueUpdated)
+
+      this.setState({
+        questions: helpcueUpdated
+      })
     })
+  }
+
+  getUserInput = (event) => {
+    this.setState({
+      userAsk: event.target.value
+    })
+  }
+
+  submit = (event) => {
+    event.preventDefault();
+
+    const dbRef = firebase.database().ref();
+    dbRef.push(this.state.userAsk)
+
+    this.setState({
+      userAsk: ''
+    })
+  }
+
+  deleteQuestion = (cueID) => {
+    console.log('deleted', cueID)
+
+    const dbRef = firebase.database().ref();
+    dbRef.child(cueID).remove()
   }
 
   render() {
     return (
       <div className="App">
-        <h1>Mock Helpcue</h1>
+        <div className="wrapper">
+          <h1 className="h1">Mock Helpcue</h1>
+          <form action="submit">
+            <div className="textBar">
+              <label htmlFor="question" className="sr-only">Please type your question here</label>
+              <textarea onChange={this.getUserInput} value={this.state.userAsk} id="question" row="20" placeholder="Type your question here" />
+            </div>
+            <button onClick={this.submit}>Help Please 😣</button>
+          </form>
+        </div>
       </div>
     );
   }
